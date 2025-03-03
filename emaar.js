7ccd2a7e2cc2c9282437a2405a11bf05d6ea558e
hamzabilalaydin <hamzaba61@gmail.com>
2025-03-03 16:56:46 +0300

    final kodu yazıldı

--- emaar.js
+++ emaar.js
@@ -3,6 +3,14 @@ const sql = require("mssql/msnodesqlv8");
 const { json } = require('express');
 
 
+// ----!!!!BU KODU ÇALIŞTIRMAK İÇİN GÜN AYARINI DEĞİŞTİRMENİZ GEREKMEKTEDİR!!!!--- //
+
+
+function roundUpToDecimal(num, decimalPlaces) {
+    const factor = Math.pow(10, decimalPlaces);
+    return Math.ceil(num * factor) / factor;
+}
+
 // Today's Date
 const today = new Date();
 const mmToday = String(today.getMonth() + 1).padStart(2, '0'); // Get month, add 1 since months are 0-based
@@ -23,16 +31,12 @@ const yyyyYesterday = yesterday.getFullYear();
 const formattedYesterday = `${mmYesterday}/${ddYesterday}/${yyyyYesterday}`;
 
 
-let gunlukCiro;
-let toplamFis;
-
-
 const config = {
     server: "localhost",
     database: "NCR",
     driver: "msnodesqlv8",
     options: {
-        trustedConnection: true // Uses Windows Authentication
+        trustedConnection: true // Uses,                                                                                                             
     }
 };
 
@@ -41,56 +45,61 @@ async function connectDB() {
         let pool = await sql.connect(config);
         console.log("Connected to MSSQL using Windows Authentication!");
 
-        gunlukCiro = await pool.request().query('SELECT COUNT(DT) AS toplamFis FROM NCR.dbo.TDT_HEADER WHERE STR = 1033 AND AMT > 0 AND DT >= CAST(DATEADD(DAY, -1, GETDATE()) AS DATE) AND DT < CAST(GETDATE() AS DATE)');
-        toplamFis = await pool.request().query('SELECT SUM(AMT) AS totalAmount FROM NCR.dbo.TDT_HEADER WHERE STR = 1033 AND AMT != 0 AND DT >= CAST(DATEADD(DAY, -1, GETDATE()) AS DATE) AND DT < CAST(GETDATE() AS DATE)');
-        
+        const toplamFis = await pool.request().query('SELECT COUNT(DT) AS Fis_Toplam FROM NCR.dbo.TDT_HEADER WHERE STR = 1033 AND AMT > 0 AND CAST(DT AS DATE) = CAST(DATEADD(DAY, -1, GETDATE()) AS DATE);');
+        const gunlukCiro = await pool.request().query('SELECT SUM(AMT) AS Satis_Toplam FROM NCR.dbo.TDT_HEADER WHERE STR = 1033 AND AMT != 0 AND CAST(DT AS DATE) = CAST(DATEADD(DAY, -1, GETDATE()) AS DATE)');
+        const toplamKDV = await pool.request().query('SELECT SUM(T_VAT) AS KDV_Toplam FROM NCR.dbo.TDT_TRX WHERE T_STR = 1033 AND T_AMOUNT != 0 AND CAST(DT AS DATE) = CAST(DATEADD(DAY, -1, GETDATE()) AS DATE)')
+
+        gunlukCiro = roundUpToDecimal(gunlukCiro.recordset[0].Satis_Toplam - toplamKDV.recordset[0].KDV_Toplam, 2).toString()
+        toplamFis = toplamFis.recordset[0].Fis_Toplam.toString()
+
         console.log({
             "PropertyCode": "ESM",
             "LeaseName": "NEZİH",
             "LeaseCode": "t0000042",
             "SaleType": "otherme",
             "SalesFromDate": formattedYesterday,
-            "NetSalesAmount": gunlukCiro.recordset[0].totalAmount.toString(),
-            "NoofTransactions": toplamFis.recordset[0].toplamFis.toString(),
+            "NetSalesAmount": gunlukCiro,
+            "NoofTransactions": toplamFis,
             "SalesFrequency": "Daily"
         })
         sql.close();
+
     } catch (err) {
         console.error("Database connection failed!", err);
     }
 }
 
 
-// function execute() {
-//     const options = {
-//         url: "https://api.emaar.com/emaar/trky/sales",
-//         method: "POST",
-//         json: [
-//             {
-//         "PropertyCode": "ESM",
-//         "LeaseName": "NEZİH",
-//         "LeaseCode": "t0000042",
-//         "SaleType": "otherme",
-//         "SalesFromDate": formattedDate,
-//         "NetSalesAmount": gunlukCiro,
-//         "NoofTransactions": fisToplam,
-//         "SalesFrequency": "Daily"
-//     }
-//     ],
-//     headers: {
-//         "x-api-key": "JfSxyPg2Qp",
-//       "Content-Type": "application/json"
-//     }
-//   };
+function execute() {
+    const options = {
+        url: "https://api.emaar.com/emaar/trky/sales",
+        method: "POST",
+        json: [
+            {
+        "PropertyCode": "ESM",
+        "LeaseName": "NEZİH",
+        "LeaseCode": "t0000042",
+        "SaleType": "otherme",
+        "SalesFromDate": formattedYesterday,
+        "NetSalesAmount": gunlukCiro,
+        "NoofTransactions": toplamFis,
+        "SalesFrequency": "Daily"
+    }
+    ],
+    headers: {
+        "x-api-key": "JfSxyPg2Qp",
+      "Content-Type": "application/json"
+    }
+  };
 
-//   request(options, function (err, res, body) {
-//       if (err) {
-//           console.error("Error:", err);
-//         } else {
-//             console.log("Response:", body);
-//         }
-//     });
-// }
+  request(options, function (err, res, body) {
+      if (err) {
+          console.error("Error:", err);
+        } else {
+            console.log("Response:", body);
+        }
+    });
+}
 
 connectDB();
 // execute();
